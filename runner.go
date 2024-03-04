f8d014557d52e5515ba4b76c7c8485b124007084
hao88 <52927295@qq.com>
2024-03-04 16:46:22 +0800

    log

--- runner.go
+++ runner.go
@@ -2,19 +2,34 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"log"
+	"os"
 	"os/exec"
 	"upgrader/pkg"
 )
 
 var (
 	IsRunning bool
+	cmd       *exec.Cmd
 )
 
 func RunScript(ctx context.Context) {
 	pkg.Log.Printf("RunScript ---")
+
+	// 如果有脚本正在运行，则先停止
+	if IsRunning {
+		stopScript()
+	}
+
+	// 标记当前脚本正在运行
 	IsRunning = true
-	// 在这里替换为你要执行的脚本和参数
-	cmd := exec.CommandContext(ctx, "./runner/app/bin/ledshowktfw")
+
+	// 初始化cmd变量
+	cmd = exec.CommandContext(ctx, "./runner/app/bin/ledshowktfw")
+	logger := log.New(os.Stdout, "cmd", log.LstdFlags)
+	// 将命令的标准输出重定向到日志记录器
+	cmd.Stdout = logger.Writer()
 	// 开始执行命令
 	if err := cmd.Start(); err != nil {
 		pkg.Log.Printf("Failed to start script: %v\n", err)
@@ -26,13 +41,11 @@ func RunScript(ctx context.Context) {
 	go func() {
 		<-ctx.Done()
 		pkg.Log.Println("Cancellation signal received, terminating script...")
-		if err := cmd.Process.Kill(); err != nil {
-			pkg.Log.Printf("Failed to kill script: %v\n", err)
-			IsRunning = true
-		} else {
-			IsRunning = false
-			pkg.Log.Printf("Script Killed")
+		if err := stopScript(); err != nil {
+			pkg.Log.Printf("Failed to stop script: %v\n", err)
 		}
+		// 标记脚本执行完成
+		IsRunning = false
 	}()
 
 	// 等待命令执行完成
@@ -41,4 +54,23 @@ func RunScript(ctx context.Context) {
 	} else {
 		pkg.Log.Println("Script execution completed successfully")
 	}
+}
+
+func stopScript() error {
+	if cmd == nil || cmd.Process == nil {
+		return nil
+	}
+
+	// 向脚本进程发送SIGTERM信号，优雅地关闭
+	if err := cmd.Process.Signal(os.Kill); err != nil {
+		return fmt.Errorf("failed to send SIGINT to process: %v", err)
+	}
+
+	// 等待脚本进程退出
+	_, err := cmd.Process.Wait()
+	if err != nil {
+		return fmt.Errorf("failed to wait for process exit: %v", err)
+	}
+
+	return nil
 }
